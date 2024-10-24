b38c25d20a8fcdbf723bd43020617785662f6806
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2024-10-24 10:20:19 +0300

    add base funcs for Pixel Robot

--- Pixel_robot_esp32_cam_core/src/main.cpp
+++ Pixel_robot_esp32_cam_core/src/main.cpp
@@ -1,18 +1,10 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "esp32_cam_mod_to_wifi_func"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  init_esp_32_cam_wifi_func();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
